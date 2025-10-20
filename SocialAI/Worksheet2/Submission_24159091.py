bc6ba56565512414baf0e4a1ec6ca2a76df99fc4
BisisthaP <patrabisistha@gmail.com>
2025-10-20 10:43:05 +0100

    Added Q1

--- SocialAI/Worksheet2/Submission_24159091.py
+++ SocialAI/Worksheet2/Submission_24159091.py
@@ -37,6 +37,36 @@ def change_velocity_follower(lead: Agent, follower: Agent, rate: float):
     follower.dx += diff_x * rate 
     follower.dy += diff_y * rate
 
+#additional function to calculate the distance between two agents
+def distance(A,B : Agent):
+    return ((A.x - B.x)**2 + (A.y - B.y)**2)**0.5 #Euclidean distance formula
+
+#part 6 
+def simulate(chg,rate):
+    leader = Agent(True) #initialise the leader 
+    followers = [Agent(False) for x in range(9)] #initialise the 9 followers 
+
+    print("Initial Positions")
+    print(f"Leader Position: ({leader.x:.2f}, {leader.y:.2f})")
+    for i in range(9):
+        print(f"Follower {i+1} Position: ({followers[i].x:.2f}, {followers[i].y:.2f})")
+    print("")
+
+    print('Simulating...')
+    for i in range(100):
+        change_velocity_lead(leader,chg) #changing the leader velocity randomly
+        leader.move(1.0) #updating the leader position
+
+        for follower in followers:
+            change_velocity_follower(leader, follower, rate) #updating follower velocity
+            follower.move(1.0) #updating follower position
+    
+    if (i + 1) % 10 == 0:
+            print("")
+            print(f"Step {i+1}: Leader Pos: ({leader.x:.2f}, {leader.y:.2f})")
+            for j in range(9):
+                print(f"  Follower {j+1}: ({followers[j].x:.2f}, {followers[j].y:.2f})")
+
 
 
 
